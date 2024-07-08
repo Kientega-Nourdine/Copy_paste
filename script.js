9473bd617d011507dde050f56b7e4d77211313df
Kientega <Kienteganourdine72@gamil.com>
2024-07-08 15:03:56 +0000

    First complete version copy_pase

--- script.js
+++ script.js
@@ -1,29 +1,75 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    // selection des elements buttons 
     const copyBtn = document.querySelector('.copy');
     const pasteBtn = document.querySelector('.paste');
-    const inputCopy = document.querySelector('.input-control input[type="text"]');
-    const inputPaste = document.querySelectorAll('.input-control input[type="text"]')[1];
+    const copyChecked = document.querySelector('.copy-checked');
+    const pasteChecked = document.querySelector('.paste-checked');
+    const restartBtn = document.querySelector('.restart');
+    const copyField = document.querySelector('.copy-field');
+    const pasteField = document.querySelector('.paste-field');
 
     // Fonction pour copier le texte
     copyBtn.addEventListener('click', async function() {
+
+        // essayer de copier le contenu du input dans le presse-papiers
         try {
-            // Copie le texte de l'input de copie dans le presse-papiers
-            await navigator.clipboard.writeText(inputCopy.value);
-            console.log('Texte copié avec succès dans le presse-papiers !');
+            await navigator.clipboard.writeText(copyField.value);
+            // console.log('Texte copié avec succès dans le presse-papiers !');
         } catch (err) {
             console.error('Erreur lors de la copie du texte : ', err);
         }
+
+        copyChecked.style.display = 'block';
+        copyBtn.style.display = 'none';
+
+        // appel de la fonction de confirmation
+        confirmed(copyBtn);
     });
 
     // Fonction pour coller le texte
     pasteBtn.addEventListener('click', async function() {
+
+        // essayer de coller le contenu dans le champ paste
         try {
-            // Colle le texte du presse-papiers dans l'input de collage
             const text = await navigator.clipboard.readText();
-            inputPaste.value = text;
-            console.log('Texte collé avec succès depuis le presse-papiers !');
+            pasteField.value = text;
+            // console.log('Texte collé avec succès depuis le presse-papiers !');
         } catch (err) {
             console.error('Erreur lors du collage du texte : ', err);
         }
+
+        pasteChecked.style.display = 'block';
+        pasteBtn.style.display = 'none';
+
+        // appel de la fonction de confirmation
+        confirmed(pasteBtn);
+    });
+
+    // Affichage de l'incone de confirmation
+    function confirmed(button) {
+
+        if(button.className.baseVal === 'copy') {
+
+            setTimeout(function () {
+                copyBtn.style.display = 'block';
+                copyChecked.style.display = 'none';
+            }, 500);
+
+        } else if (button.className.baseVal === 'paste') {
+
+            setTimeout(function () {
+                pasteBtn.style.display = 'block';
+                pasteChecked.style.display = 'none';
+            }, 500);
+
+        } else {
+            window.location.href = '/';
+        }
+    }
+
+    // Vide le contenu du premier input
+    restartBtn.addEventListener('click', function() {
+        copyField.value = ''; 
     });
 });
